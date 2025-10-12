a90f04c330714a095590167003b85e91ae55a4be
nik <102893782+nmalthouse@users.noreply.github.com>
2025-10-11 22:08:32 -0500

    wip of vpk editor

--- src/windows/asset.zig
+++ src/windows/asset.zig
@@ -26,23 +26,10 @@ pub const AssetBrowser = struct {
     cbhandle: guis.CbHandle = .{},
     area: iArea,
 
-    ed: *Context,
-    should_exit: bool = false,
+    tex_browse: TextureBrowser,
     alloc: std.mem.Allocator,
 
-    mat_list: ArrayList(VpkId) = .{},
-    mod_list: ArrayList(VpkId) = .{},
-
-    mat_list_search_a: ArrayList(VpkId) = .{},
-    mat_list_search_b: ArrayList(VpkId) = .{},
-
-    mod_list_search: ArrayList(VpkId) = .{},
-
-    num_column: usize = 12,
-
-    prev_search: ArrayList(u8) = .{},
-
-    scr_ptr: ?*Wg.VScroll = null,
+    ed: *Context,
 
     pub fn create(gui: *Gui, editor: *Context) !*AssetBrowser {
         const self = gui.create(@This());
@@ -51,6 +38,7 @@ pub const AssetBrowser = struct {
             .vt = iWindow.init(&@This().build, gui, &@This().deinit, &self.area),
             .ed = editor,
             .alloc = gui.alloc,
+            .tex_browse = .{ .alloc = gui.alloc, .ed = editor, .win = &self.vt },
         };
 
         return self;
@@ -83,11 +71,11 @@ pub const AssetBrowser = struct {
                         }
                     }
                 }
-                try self.mat_list.append(self.alloc, item.key_ptr.*);
+                try self.tex_browse.mat_list.append(self.alloc, item.key_ptr.*);
             } else if (id == mdl) {
-                try self.mod_list.append(self.alloc, item.key_ptr.*);
+                try self.tex_browse.mod_list.append(self.alloc, item.key_ptr.*);
             } else if (id == png) {
-                try self.mat_list.append(self.alloc, item.key_ptr.*);
+                try self.tex_browse.mat_list.append(self.alloc, item.key_ptr.*);
             }
         }
         log.info("excluded {d} materials", .{excluded});
@@ -95,11 +83,7 @@ pub const AssetBrowser = struct {
 
     pub fn deinit(vt: *iWindow, gui: *Gui) void {
         const self: *@This() = @alignCast(@fieldParentPtr("vt", vt));
-        self.mod_list.deinit(self.alloc);
-        self.mat_list.deinit(self.alloc);
-        self.mat_list_search_a.deinit(self.alloc);
-        self.mat_list_search_b.deinit(self.alloc);
-        self.prev_search.deinit(self.alloc);
+        self.tex_browse.deinit();
         vt.deinit(gui);
         gui.alloc.destroy(self); //second
     }
@@ -115,10 +99,171 @@ pub const AssetBrowser = struct {
         self.area.area = area;
         self.area.clearChildren(gui, win);
         self.area.dirty(gui);
-        self.scr_ptr = null;
+        self.tex_browse.reset();
         const inset = GuiHelp.insetAreaForWindowFrame(gui, win.area.area);
         const lay = &self.area;
-        var ly = guis.VerticalLayout{ .padding = .{}, .item_height = gui.style.config.default_item_h, .bounds = inset };
+
+        self.tex_browse.build(lay, win, gui, inset);
+    }
+};
+
+const VpkBrowser = struct {
+    const Self = @This();
+    alloc: std.mem.Allocator,
+    ed: *Context,
+    win: *iWindow,
+
+    cbhandle: guis.CbHandle = .{},
+
+    list_search_a: ArrayList(VpkId) = .{},
+    list_search_b: ArrayList(VpkId) = .{},
+
+    prev_search: ArrayList(u8) = .{},
+    scr_ptr: ?*Wg.VScroll = null,
+
+    pub fn deinit(self: *@This()) void {
+        self.list_search_a.deinit(self.alloc);
+        self.list_search_b.deinit(self.alloc);
+        self.prev_search.deinit(self.alloc);
+    }
+
+    pub fn reset(self: *@This()) void {
+        self.scr_ptr = null;
+    }
+
+    pub fn build(self: *@This(), lay: *iArea, win: *iWindow, gui: *Gui, area: Rect) void {
+        const sp = area.split(.vertical, area.w / 2);
+        var ly = guis.VerticalLayout{ .padding = .{}, .item_height = gui.style.config.default_item_h, .bounds = sp[0] };
+        if (ly.getArea()) |header| {
+            const header_col = 4;
+            var hy = guis.HorizLayout{ .bounds = header, .count = header_col };
+            if (guis.label(lay, gui, win, hy.getArea(), "Search", .{})) |ar| {
+                if (Wg.Textbox.buildOpts(gui, ar, .{
+                    .user_id = 0,
+                    .commit_vt = &self.cbhandle,
+                    .commit_cb = cb_commitTextbox,
+                    .commit_when = .on_change,
+                })) |tbvt| {
+                    lay.addChildOpt(gui, win, tbvt);
+                    gui.grabFocus(tbvt.vt, win);
+                }
+            }
+        }
+        _ = ly.getArea(); //break
+
+        self.list_search_a.clearRetainingCapacity();
+        self.list_search_a.appendSlice(self.alloc, self.mat_list.items) catch {};
+
+        ly.pushRemaining();
+        if (ly.getArea()) |tview| {
+            if (Wg.VScroll.build(gui, tview, .{
+                .build_cb = buildVpkList,
+                .build_vt = &self.cbhandle,
+                .win = win,
+                .count = self.list_search_a.items.len,
+                .item_h = gui.style.config.default_item_h,
+            })) |scr| {
+                lay.addChildOpt(gui, win, scr);
+                self.scr_ptr = @alignCast(@fieldParentPtr("vt", scr.vt));
+            }
+        }
+
+        // Main view
+        // info about matches
+        // maybe toggles or something
+        // search bar
+
+    }
+
+    fn cb_commitTextbox(cb: *CbHandle, gui: *Gui, string: []const u8, _: usize) void {
+        const self: *@This() = @alignCast(@fieldParentPtr("cbhandle", cb));
+        if (std.mem.eql(u8, string, self.prev_search.items))
+            return;
+        defer {
+            self.prev_search.clearRetainingCapacity();
+            self.prev_search.appendSlice(self.alloc, string) catch {};
+        }
+
+        var search_list = self.mat_list.items;
+
+        if (std.mem.startsWith(u8, string, self.prev_search.items)) {
+            self.mat_list_search_b.clearRetainingCapacity();
+            self.mat_list_search_b.appendSlice(self.alloc, self.mat_list_search_a.items) catch return;
+            search_list = self.mat_list_search_b.items;
+        }
+        const io = std.mem.indexOf;
+        self.mat_list_search_a.clearRetainingCapacity();
+        for (search_list) |item| {
+            const tt = self.ed.vpkctx.entries.get(item) orelse continue;
+            if (io(u8, tt.path, string) != null or io(u8, tt.name, string) != null)
+                self.mat_list_search_a.append(self.alloc, item) catch return;
+        }
+        if (self.scr_ptr) |scr| {
+            scr.updateCount(self.getTextureRowCount());
+            scr.index_ptr.* = 0;
+            scr.rebuild(gui, self.win);
+        }
+    }
+
+    fn buildVpkList(cb: *CbHandle, vt: *iArea, index: usize, gui: *Gui, win: *iWindow) void {
+        const self: *@This() = @alignCast(@fieldParentPtr("cbhandle", cb));
+        const adj_index = index * self.num_column;
+        if (adj_index >= self.mat_list_search_a.items.len) return;
+        var tly = guis.TableLayout{
+            .columns = @intCast(self.num_column),
+            .item_height = self.getTextureRowHeight(vt.area.w),
+            .bounds = vt.area,
+        };
+        for (self.mat_list_search_a.items[adj_index..]) |mat| {
+            const tt = self.ed.vpkctx.entries.get(mat) orelse return;
+            const tint: u32 = if (mat == self.ed.asset_browser.selected_mat_vpk_id) 0xff8888_ff else 0xffff_ffff;
+            vt.addChildOpt(gui, win, ptext.PollingTexture.build(gui, tly.getArea(), self.ed, mat, "{s}/{s}", .{
+                tt.path, tt.name,
+            }, .{
+                .cb_vt = cb,
+                .cb_fn = void,
+                .id = mat,
+                .tint = tint,
+            }));
+        }
+    }
+};
+
+const TextureBrowser = struct {
+    const Self = @This();
+    alloc: std.mem.Allocator,
+    ed: *Context,
+    win: *iWindow,
+
+    cbhandle: guis.CbHandle = .{},
+
+    mat_list: ArrayList(VpkId) = .{},
+    mod_list: ArrayList(VpkId) = .{},
+
+    mat_list_search_a: ArrayList(VpkId) = .{},
+    mat_list_search_b: ArrayList(VpkId) = .{},
+
+    mod_list_search: ArrayList(VpkId) = .{},
+
+    num_column: usize = 12,
+
+    prev_search: ArrayList(u8) = .{},
+
+    scr_ptr: ?*Wg.VScroll = null,
+    pub fn deinit(self: *@This()) void {
+        self.mod_list.deinit(self.alloc);
+        self.mat_list.deinit(self.alloc);
+        self.mat_list_search_a.deinit(self.alloc);
+        self.mat_list_search_b.deinit(self.alloc);
+        self.prev_search.deinit(self.alloc);
+    }
+
+    pub fn reset(self: *@This()) void {
+        self.scr_ptr = null;
+    }
+
+    pub fn build(self: *@This(), lay: *iArea, win: *iWindow, gui: *Gui, area: Rect) void {
+        var ly = guis.VerticalLayout{ .padding = .{}, .item_height = gui.style.config.default_item_h, .bounds = area };
         if (ly.getArea()) |header| {
             const header_col = 4;
             var hy = guis.HorizLayout{ .bounds = header, .count = header_col };
@@ -157,6 +302,7 @@ pub const AssetBrowser = struct {
         // info about matches
         // maybe toggles or something
         // search bar
+
     }
 
     fn getTextureRowCount(self: *const Self) usize {
@@ -193,7 +339,7 @@ pub const AssetBrowser = struct {
         if (self.scr_ptr) |scr| {
             scr.updateCount(self.getTextureRowCount());
             scr.index_ptr.* = 0;
-            scr.rebuild(gui, gui.getWindow(&self.area) orelse return);
+            scr.rebuild(gui, self.win);
         }
     }
 
